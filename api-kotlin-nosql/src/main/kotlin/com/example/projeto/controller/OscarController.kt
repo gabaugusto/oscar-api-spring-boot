3d569c3d964f808097f342dcf99c2a65770f50a6
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2025-05-26 13:06:52 -0300

    Update OscarController.kt

--- api-kotlin-nosql/src/main/kotlin/com/example/projeto/controller/OscarController.kt
+++ api-kotlin-nosql/src/main/kotlin/com/example/projeto/controller/OscarController.kt
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Sort
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@CrossOrigin(origins = ["*"]) // Permite requisições de qualquer origem
 @RestController
 @RequestMapping("/api/indicacoes")
 class OscarController(
