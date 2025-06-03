537fc650f937bd4507eee89feb0f2565c14afe2b
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2025-06-03 05:42:06 -0300

    Theme

--- interface-mobile/app/src/main/java/com/sample/oscarinterface/ui/theme/Type.kt
+++ interface-mobile/app/src/main/java/com/sample/oscarinterface/ui/theme/Type.kt
@@ -1,19 +1,36 @@
 package com.sample.oscarinterface.ui.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sample.oscarinterface.R
+
+val textLarge = TextStyle(
+    fontFamily = FontFamily(Font(R.font.montserrat)),
+    fontWeight = FontWeight.W600,
+    fontSize = 36.sp,
+    lineHeight = 28.sp,
+    letterSpacing = 0.sp
+)
 
 // Set of Material typography styles to start with
-val Typography = Typography(
+@Composable
+fun ProvideTypography(): Typography{
+    return Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.montserrat)), // Use Montserrat for body text
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        color = MaterialTheme.colorScheme.primary,
+
+        letterSpacing = 0.5.sp,
+    )
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
@@ -31,4 +48,4 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
-)
+}
