1e994f312cda59c976e1f832b07944bb94328f5e
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2025-06-03 05:41:16 -0300

    Themes

--- interface-mobile/app/src/main/java/com/sample/oscarinterface/ui/theme/Color.kt
+++ interface-mobile/app/src/main/java/com/sample/oscarinterface/ui/theme/Color.kt
@@ -2,6 +2,24 @@ package com.sample.oscarinterface.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Colors used in the Oscar Interface theme
+// Primary colors
+val Dark = Color(0xFF171725)
+val SoftDark = Color(0xFF696974)
+val BlueAccent = Color(0xFF12CDD9)
+val BackgroundColor = Color(0xFF1F1D2B)
+
+// Secondary colors
+val Green = Color(0xFF22B07D)
+val Orange = Color(0xFFFF8700)
+val Red = Color(0xFFFB4141)
+
+// Text Colors
+val TextBlack = Color(0xFF171725)
+val TextDarkGray = Color(0xFF696974)
+val TextLightGray = Color(0xFF92929D)
+val TextWhite = Color(0xFFFF1F1F5)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
--- interface-mobile/app/src/main/java/com/sample/oscarinterface/ui/theme/Theme.kt
+++ interface-mobile/app/src/main/java/com/sample/oscarinterface/ui/theme/Theme.kt
@@ -1,9 +1,9 @@
 package com.sample.oscarinterface.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -12,25 +12,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BlueAccent,
+    secondary = SoftDark,
+    tertiary = Orange,
+    background = BackgroundColor,
+    surface = BackgroundColor,
+    onPrimary = BlueAccent,
+    onSecondary = SoftDark,
+    onTertiary = Orange,
+    onBackground = BackgroundColor,
+    onSurface = BackgroundColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = BlueAccent,
+    secondary = SoftDark,
+    tertiary = Orange,
+    background = BackgroundColor,
+    surface = BackgroundColor,
+    onPrimary = BlueAccent,
+    onSecondary = SoftDark,
+    onTertiary = Orange,
+    onBackground = BackgroundColor,
+    onSurface = BackgroundColor
 )
 
 @Composable
@@ -52,7 +56,7 @@ fun OscarInterfaceTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = ProvideTypography(),
         content = content
     )
 }
